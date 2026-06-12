ac1ff085b653c39f0d89936b67ab6f548f7aa27c
Samuel Bush <sam.j.bush@outlook.com>
2026-06-12 21:29:37 +0100

    Refine output format and time interval for projectile

--- projectile-simulator/projectile.py
+++ projectile-simulator/projectile.py
@@ -4,10 +4,11 @@ y = 0 # Y must be established before while loop as it is included in the conditi
 time = 0
 speed = int(input("Enter launch speed (m/s): "))
 angle = int(input("Enter launch angle (deg): "))
+timeInterval = float(input("Enter position output interval (s, generally 0.01 - 1): ")
 radians = math.radians(angle) # Python sin and cos accept values in radian so conversion is needed
 
 while y >=  0: # So the co-ordinates stop outputting when projectile hits the 'ground'
   x = speed * math.cos(radians) * time # Calculate with general equation for x co-ordinate of projectile
   y = speed * math.sin(radians) * time - (0.5 * 9.8 * time**2) # Calculate with general equation for y co-ordinate of projectile
-  print(f"[{x},{y}]")
-  time = time + 0.2 # Co-ordinate output interval is 0.2s to balance detail needed and amount of outputs
+  print(f"[{round(x,2)},{round(y,2)}]")
+  time = time + timeInterval # Co-ordinate output interval is dertemined by user input on line 7
